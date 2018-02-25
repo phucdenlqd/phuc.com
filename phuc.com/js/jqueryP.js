f0c8b8f4bb262d20ae2a7dfec0c6c7f492f3cd41
NGUYEN Phuc <phucdenlqd@gmail.com>
2018-02-25 03:24:15 +0100

    my first commit

---
+++ phuc.com/js/jqueryP.js
@@ -0,0 +1,14 @@
+
+// dong bo hoa cac hang
+$(function() {
+    $('div[class*="col-"]').matchHeight({
+    byRow: true,
+    property: 'height',
+    target: null,
+    remove: false
+});
+});
+
+
+
+
